d6f8484db5bdecf7a9c353657ac3c7e860045dbc
mohitosmollick <69431291+mohitosmollick@users.noreply.github.com>
2023-02-26 01:50:49 +0600

    sdfa

--- src/componet/Search.jsx
+++ src/componet/Search.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 const Search = () =>{
     return(
-        <>
-            <>
-                HomePage
-            </>
-        </>
+        <div className="search">
+            <div className="searchForm">
+                <input type="text" />
+            </div>
+            <div className="userChat">
+                <img src="" alt="" />
+            </div>
+        </div>
     )
 }
 export default Search;
