94adc5534ce9452aa5b770460dd28a656bcbb3f3
mohitosmollick <69431291+mohitosmollick@users.noreply.github.com>
2023-02-27 02:14:35 +0600

    firebase working

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBoPPpEIaLLIyJcH8C2If_Mdv5_s27KK8A",
   authDomain: "react-chat-application-b9377.firebaseapp.com",
@@ -14,4 +14,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
+export const storage = getStorage();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { FcAddImage } from 'react-icons/fc';
 import { Link } from "react-router-dom";
+import {createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {auth, storage} from '../firebase';
+
+import {ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+
 
 const Register = () =>{
+    const [err, setError] = useState(false);
+    const handleSubmit = async (e)=>{
+        e.preventDefault()
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+
+        
+    try{
+    const res = await createUserWithEmailAndPassword(auth, email, password)  
+const storageRef = ref(storage, displayName);
+
+const uploadTask = uploadBytesResumable(storageRef, file);
+uploadTask.on('state_changed', 
+  (snapshot) => {
+
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+    }
+  }, 
+  (error) => {
+
+  }, 
+  () => {
+  
+    getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+    await updateProfile(res.user,{
+        displayName,
+        photoURL: downloadURL,
+    })
+    });
+  }
+);
+    }catch(err){
+        setError(true)
+    }
+
+  
+}
     return(
         <div className="fromContainer">
             <div className="formWrapper">
                 <span className="logo">Live Chat</span>
                 <div className="title">Register</div>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="display name" />
                     <input type="email" placeholder="eamil" />
                     <input type="password" placeholder="password" />
@@ -17,6 +70,8 @@ const Register = () =>{
                     <span>Add to Avatar</span>
                     </label>
                     <button>Sign Up</button>
+
+                    {err && <span>Something wrong!</span>}
                 </form>
                 <p>Do you have any account? <Link to='/login'>Login</Link> </p>
             </div>
